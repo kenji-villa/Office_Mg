3bf96f6f2ef1ac193e86c4326b14e3ca5cf781f4
eziosystem32 <teen2000titans@gmail.com>
2024-05-18 17:44:54 +0300

    Done

--- Assighn.cpp
+++ Assighn.cpp
@@ -29,13 +29,66 @@ void addEmp(Employee& record) {
     cin>>record.position;
 }
 
-void searchEmp(Employee& record, fstream& file){
-    
+    bool searchEmp(fstream& file) {
+    string empName;
+    cout<<"pleas enter employee name";
+    cin>> empName;
+string line;
+
+//a while loop is used to read each line from the file using the getline function. The loop continues until there are no more lines to read from the file.
+while (getline(file, line)) {
+//the code finds the position of the comma (',') using the find function of the string class. The position of the comma indicates the end of the employee name.  
+size_t pos = line.find(',');
+//The substr function is then used to extract the portion of the line from the beginning (index 0) up to the position of the comma.
+string name = line.substr(0, pos);
+//The extracted name is compared with the empName parameter to check if it matches. If a match is found, the code displays a message indicating that the employee is found
+if (name == empName) {
+cout << "Employee found!" << endl;
+cout << line << endl;
+return true;
+}
+}
+cout << "Employee not found." << endl;
+return false;
 }
 
-void deleteEmp(Employee& record, fstream& file) {
 
-
+void deleteEmp(fstream& file) {
+    string empName;
+    cout<<"pleas enter employee name";
+    cin>> empName;
+//The temporary file will be used to store the employee records except for the one to be deleted.  
+    fstream tempFile("temp.txt", ios::out);
+    string line;
+//Found variable will be used to determine if the employee to be deleted is found in the file.
+    bool found = false;
+//The loop continues until there are no more lines to be read
+    while (getline(file, line)) {
+//It extracts the name of the employee from the line by using the substr function
+        size_t pos = line.find(',');
+//The extracted name is stored in the variable name
+        string name = line.substr(0, pos);
+//It checks if the extracted name (name) is not equal to the empName (the name of the employee to be deleted).
+        if (name != empName) {
+//If they are not equal, it means this line does not correspond to the employee to be deleted.
+//so it writes the line to the tempFile using the << operator.
+            tempFile << line << endl;
+//In this case, it sets the found variable to true to indicate that the employee has been found but does not write this line to the tempFile.
+        } else {
+            found = true;
+        }
+    }
+    file.close();
+    tempFile.close();
+//It uses the remove function to delete the original file "employees.txt"
+    remove("employees.txt");
+//It uses the rename function to rename the temporary file "temp.txt" to "employees.txt". 
+    rename("temp.txt", "employees.txt");
+    if (found) {
+        cout << "Employee deleted successfully." << endl;
+    } else {
+        cout << "Employee not found." << endl;
+    }
 }
 
 void saveEmp(const Employee& record, fstream& outFile) {
@@ -50,9 +103,17 @@ void editEmp(Employee& record, fstream& file){
 }
 
 
-void readRecords(fstream& inFile) {
+void readRecords(fstream& file) {
+    string line;
+    cout << "Employee Records:" << endl;
+    while (getline(file, line)) {
+        cout << line << endl;
+    
+}
 
 }
+
+
 
 
 void display(fstream &record) {
@@ -90,10 +151,10 @@ int main() {
         editEmp(*record, file);
         break;
         case 3:
-        deleteEmp(*record, file);
+        deleteEmp(file);
         break;
         case 4:
-        searchEmp(*record, file);
+        searchEmp(file);
         break;
         case 5:
         display(file);
